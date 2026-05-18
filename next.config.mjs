834bece63e8007aaa6f5eceb33681af3dbd83f1a
magicaiweb <yasseraiassistant@gmail.com>
2026-05-18 19:48:39 +0200

    Fix preview asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/websites/wayyak',
+  assetPrefix: '/websites/wayyak',
   images: { unoptimized: true },
 }
 
